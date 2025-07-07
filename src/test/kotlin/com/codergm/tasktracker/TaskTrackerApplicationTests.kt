1c92fcba4cc5239e38c2aab000302cf8211cb72b
askidme <id.askarov@gmail.com>
2025-07-06 17:58:31 -0700

    init

---
+++ src/test/kotlin/com/codergm/tasktracker/TaskTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codergm.tasktracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
